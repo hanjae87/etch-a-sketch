9eb92b885d36f3139c6580ae84701a38f2caf640
HanJae Lee <hanjae87@gmail.com>
2016-07-15 15:29:52 +0900

    all

--- Script/jquery.js
+++ Script/jquery.js
@@ -1,19 +1,27 @@
 $(document).ready(function() {
 	$("#button").click(function() {
+		var rowInput = $("input[name=row]").val();
+		var columnInput = $("input[name=column]").val();
+
+		var pixelWidth = 1500/columnInput;
+		var pixelHeight = 1500/rowInput;
+
 		$(".columnPixel").remove();
 		for(var columnNumber = 0; 
-			columnNumber < $("input[name='column']").val(); 
+			columnNumber < columnInput; 
 			columnNumber++) {
 			$(".wrap").append("<div class='columnPixel'></div>")
 		};
+		$(".columnPixel").css({"width": pixelWidth, "height": pixelHeight});
 		$(".rowPixel").remove();
 		for(var rowNumber = 0; 
-			rowNumber < $("input[name='row']").val(); 
+			rowNumber < rowInput; 
 			rowNumber++) {
 			$(".columnPixel").append("<div class='rowPixel'></div>");
+		$(".rowPixel").css({"width": pixelWidth, "height": pixelHeight});
 		};
 	});
-	$(".rowPixel").mouseover(function() {
-		$(this).switchClass("color");
+	$("div").on("mouseover", ".rowPixel", function() {
+		$(this).addClass("color")
 	});
 });
